c39f2e5a0670b75a4e5c290eb3716e4a5f6e4bc0
Benjiejr <benjamisaac2019@gmail.com>
2023-04-13 20:15:25 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  const [count, setCount] = React.useState(4);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
